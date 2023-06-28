aa7b99a52ed729e79654b0edff89979c024e05bf
harshilpatel481 <82863853+harshilpatel481@users.noreply.github.com>
2023-06-28 23:13:09 +0530

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,7 +4,7 @@
 const projects = [
 	{
 		id: 1,
-		title: 'Name Health Platforms',
+		title: 'Name Name Platforms',
 		category: 'Web Application',
 		img: require('@/assets/images/web-project-2.jpg'),
 	},
